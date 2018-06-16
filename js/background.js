7e012ee1838d694ec7954deb923e24cf779d25ca
hmirin <foo@example.com>
2018-06-16 21:36:26 +0900

    formatting

--- js/background.js
+++ js/background.js
@@ -116,10 +116,10 @@ function google_cloud_tts_error_handler(err) {
     console.error(err);
 }
 
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason == "install") {
         chrome.tabs.create({ url: "https://hmirin.github.io/speechy/installed" });
-    }else if(details.reason == "update"){
+    } else if (details.reason == "update") {
         chrome.tabs.create({ url: "https://hmirin.github.io/speechy/installed" });
     }
 });

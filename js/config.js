9c52bc966ea9e832785b5b577ad558e399d90ae0
hmirin <panda.1192@gmail.com>
2024-12-07 23:39:01 +0900

    lint and format

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 const SPEECHY_CONFIG = {
-    ERROR_MESSAGE: "Couldn't retrieve the selected text. \nNote: Speechy won't work on PDFs, urls starts with chrome:// and Chrome app store, because of the limit of Chrome's API.",
-    DEFAULT_OPTIONS: {
-        api_provider: "Google",
-        openai_apikey: "",
-        google_apikey: "",
-        openai_voice: "alloy",
-        openai_model: "tts-1",
-        google_voice: "en-US-Wavenet-D",
-        google_speed: 1
-    }
+  ERROR_MESSAGE:
+    "Couldn't retrieve the selected text. \nNote: Speechy won't work on PDFs, urls starts with chrome:// and Chrome app store, because of the limit of Chrome's API.",
+  DEFAULT_OPTIONS: {
+    api_provider: "Google",
+    openai_apikey: "",
+    google_apikey: "",
+    openai_voice: "alloy",
+    openai_model: "tts-1",
+    google_voice: "en-US-Wavenet-D",
+    google_speed: 1,
+  },
 };
